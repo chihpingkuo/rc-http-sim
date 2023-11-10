dabef3fbc5baa2b6c300cf569dfdd2baa309436b
chihpingkuo <chihpingkuo@msn.com>
2023-11-10 14:37:43 +0800

    parse artisan alog

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,29 @@ use axum::{
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use std::{fs::File, io::Read, net::SocketAddr};
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
-
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root));
+
+    let mut file_content = String::new();
+    let mut file = File::open("23-11-05_1013.alog").unwrap();
+    file.read_to_string(&mut file_content).unwrap();
+
+    // change some character before parse
+    let json_str = file_content
+        .replace("\"", "\\\"")
+        .replace("'", "\"")
+        .replace("False", "false")
+        .replace("True", "true");
+
+    let result: serde_json::Value = serde_json::from_str(&json_str).unwrap();
+
+    println!("{:?}", result.as_object().unwrap().get("timex"));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -23,12 +35,19 @@ async fn main() {
 }
 
 // basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn root() -> (StatusCode, Json<Metrics>) {
+    let metrics = Metrics {
+        BT: 20.0,
+        ET: 20.0,
+        inlet: 40.0,
+    };
+
+    (StatusCode::OK, Json(metrics))
 }
 
 #[derive(Serialize)]
 struct Metrics {
-    id: u64,
-    username: String,
+    BT: f32,
+    ET: f32,
+    inlet: f32,
 }
